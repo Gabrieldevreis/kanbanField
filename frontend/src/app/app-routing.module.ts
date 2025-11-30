613ac81618ac592637e1cab08770a33f4d5dc9b7
gabrieldevreis <gabrielreisdev@gmail.com>
2025-11-30 07:46:05 -0300

    feat:adicionado rota core

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) }, { path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
