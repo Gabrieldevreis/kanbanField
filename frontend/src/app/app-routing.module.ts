5059af4f7a7135968f075c47b2979da4205570d1
gabrieldevreis <gabrielreisdev@gmail.com>
2025-12-07 19:02:10 -0300

    refactor: atualizar app-routing e app.module para nova organização de páginas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () =>
-      import('./pages/pages.module').then((m) => m.PagesModule),
-  },
-  { path: '**', redirectTo: '' },
-];
+    path:'',
+    component:LoginComponent,
+  }
+]
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
