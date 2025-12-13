4adc396735894f3b6b97fbfa0e977b56e78e19a6
gabrieldevreis <gabrielreisdev@gmail.com>
2025-12-13 19:41:32 -0300

    feat(home): centralize features and update routing after removing old pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { KanbanComponent } from './shared/components/kanban/kanban.component';
+import { CalendarComponent } from './shared/components/calendar/calendar.component';
+import { ConfigurationsComponent } from './shared/components/configurations/configurations.component';
+import { BoardsComponent } from './shared/components/boards/boards.component';
 
 const routes: Routes = [
   {
@@ -16,6 +21,28 @@ const routes: Routes = [
   {
     path:'register',
     component:RegisterComponent
+  },
+  {
+    path:'',
+    component:HomeComponent,
+    children:[
+      {
+        path:'initial',
+        component:KanbanComponent
+      },
+      {
+        path:'calendar',
+        component:CalendarComponent
+      },
+      {
+        path:'configuration',
+        component:ConfigurationsComponent
+      },
+      {
+        path:'boards',
+        component:BoardsComponent
+      }
+    ]
   }
 ]
   
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,29 @@ import { AppComponent } from './app.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component'
+import { RegisterComponent } from './pages/register/register.component';
+import { SiderbarComponent } from './shared/components/siderbar/siderbar.component';
+import { BoardsComponent } from './shared/components/boards/boards.component';
+import { KanbanComponent } from './shared/components/kanban/kanban.component';
+import { ConfigurationsComponent } from './shared/components/configurations/configurations.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { CalendarComponent } from './shared/components/calendar/calendar.component';
+import { HeaderComponent } from './shared/components/header/header.component'
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,   
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    SiderbarComponent,
+    BoardsComponent,
+    KanbanComponent,
+    ConfigurationsComponent,
+    FooterComponent,
+    CalendarComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
